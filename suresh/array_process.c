02ffdeeb8ffe8f96853f0ee9fc025b270945c0bb
BPYDIRAJU <raju.baliboyina@gmail.com>
2025-06-23 17:41:06 +0530

    Add GitHub Actions workflow

---
+++ suresh/array_process.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int arr[10];
+	int i,n;i      
+	n=sizeof(arr)/sizeof a[0];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d \n",arr);
+	}
+}
